5b6bc1f4fe4280bcd6a9878e4f8357ee858a5a10
Mai200611 <penagarciam061@gmail.com>
2026-05-15 12:13:27 -0500

    Segundo commit con configuración en Launchsettings.json

---
+++ Distribuidora.API/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
